f4c4687ec4bbb71578592e5ec1cb35798402e4e1
Johnatan Lima <joh23nathan@gmail.com>
2018-12-04 22:27:17 -0200

    Serializando dados com Json

---
+++ SerializingJson/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SerializingJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criando um objeto Produto
+            var produto1 = new Produto(){ id = 100, nome = "Mesa", preco = 10.99};
+    
+        }
+    }
+
+    //Criando uma classe modelo
+    class Produto{
+        public int id { get; set; }
+        public string nome { get; set; }
+        public double preco { get; set; }
+    }
+}
