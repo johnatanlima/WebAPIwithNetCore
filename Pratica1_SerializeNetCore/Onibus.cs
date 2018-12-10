250dfd79a7008dd32f28b917ed814345e466908c
User Dexter <UserDexter@dexter.localdomain>
2018-12-10 02:49:13 -0200

    Exercitando uso dos Obj Json com Csharp - Foco Web API's

---
+++ Pratica1_SerializeNetCore/Onibus.cs
@@ -0,0 +1,9 @@
+namespace Pratica1_SerializeNetCore {
+    
+    public class Onibus{
+        public int ID { get; set; } 
+        public string Empresa { get; set; } 
+        public string  Destino { get; set; }
+        public double VelMax { get; set; }
+    }
+}
---
+++ Pratica1_SerializeNetCore/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Pratica1_SerializeNetCore{
+    class Program
+    {
+        static void Main(string[] args){
+
+            var arraySerializado = DoSerialization();
+            System.Console.WriteLine(arraySerializado);
+            System.Console.WriteLine(" ======================== ");
+
+            System.Console.WriteLine("  VOILÀAAAAAA ...     ");
+            DoDeserialization(arraySerializado);
+            
+
+
+        }       
+
+            // Serialize o array de onibus para uma string Json. 
+        public static string DoSerialization() {
+       
+        Onibus[] variosOnibus = {
+            new Onibus{ ID = 503, Empresa = "ViaAgil", Destino = "Parque Tecnológico", VelMax=80.5},
+            new Onibus{ ID = 505, Empresa = "ViaAgil", Destino = "Centro de Inovação", VelMax=90.5},
+            new Onibus{ ID = 500, Empresa = "ViaAgil", Destino = "Trabalho", VelMax=110.5},
+            new Onibus{ ID = 240, Empresa = "ViaAgil", Destino = "CKP ", VelMax = 70.2}
+        };
+   
+            var jsonVariosBus = JsonConvert.SerializeObject(variosOnibus);
+        
+            return jsonVariosBus;
+        }
+
+        // Deserialize a string Json, para um array de objetos do tipo Onibus. 
+        public static void DoDeserialization(string jsonVariosBus) {
+    
+            var severalBus = JsonConvert.DeserializeObject<Onibus[]>(jsonVariosBus);
+    
+                foreach (var onibusEncontrado in severalBus) {
+                    System.Console.WriteLine($"ID:{onibusEncontrado.ID} Empresa:{onibusEncontrado.Empresa} Destino:{onibusEncontrado.Destino} VelMax:{onibusEncontrado.VelMax}");
+                 }
+            
+            }
+
+    }//ClassProgram
+
+}
