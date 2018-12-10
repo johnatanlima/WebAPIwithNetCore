049016732d7403ae6f4e56ec18ba7a027a16046b
User Dexter <UserDexter@dexter.localdomain>
2018-12-10 00:47:56 -0200

    Proxima Atividade

---
+++ WebServerAndWebClient_UnicaMaquina/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebServerAndWebClient_UnicaMaquina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
